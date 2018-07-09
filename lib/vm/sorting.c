4af4e4a292140a26f79e5c58515d5e7e8b091416
Silvio Bacci <s.bacci@outlook.it>
2018-07-09 22:45:22 +0200

    Update of WF sorting algorithm

--- lib/vm/sorting.c
+++ lib/vm/sorting.c
@@ -54,19 +54,10 @@ void sort_by_increasing_empty_space(vm *v) {
 //------------------------------------------------------------------------------
 
 void sort_by_decreasing_empty_space(vm *v) {
-	unsigned int i, j, n_cpus = 0;
+	unsigned int i, j;
 	cpu * temp = malloc(sizeof(cpu));
 
-	sort_by_increasing_empty_space(v);
-
-	for (i = 0; i < v->n_cpus; i++)
-		if(v->cpus[i].u > 0)
-			n_cpus++;
-
-	if(n_cpus == 0)
-		n_cpus++;
-	
-	for (i = n_cpus - 1; i > 0; i--)
+	for (i = v->n_cpus - 1; i > 0; i--)
 		for (j = 0; j < i; j++)
 			if (v->cpus[j].u > v->cpus[j + 1].u) {
 				*temp = v->cpus[j + 1];
@@ -99,19 +90,10 @@ void sort_by_increasing_empty_space_workload(vm *v) {
 //------------------------------------------------------------------------------
 
 void sort_by_decreasing_empty_space_workload(vm *v) {
-	unsigned int i, j, n_cpus = 0;
+	unsigned int i, j;
 	cpu * temp = malloc(sizeof(cpu));
 
-	sort_by_increasing_empty_space_workload(v);
-
-	for (i = 0; i < v->n_cpus; i++)
-		if(v->cpus[i].ts->size > 0)
-			n_cpus++;
-
-	if(n_cpus == 0)
-		n_cpus++;
-	
-	for (i = n_cpus - 1; i > 0; i--)
+	for (i = v->n_cpus - 1; i > 0; i--)
 		for (j = 0; j < i; j++)
 			if (utilization_factor_modified(v->cpus[j].ts) > utilization_factor_modified(v->cpus[j + 1].ts)) {
 				*temp = v->cpus[j + 1];
